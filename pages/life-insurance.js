7f6c7d7b288381a33f7cc9e4e4145802538b4bf7
spdydve <dev.dpkennedy@gmail.com>
2021-01-15 08:12:01 -0500

    Added client loading of policy genius script

--- pages/life-insurance.js
+++ pages/life-insurance.js
@@ -1,4 +1,5 @@
 const { default: Layout } = require('@components/Layout');
+import { useEffect } from 'react';
 import { Grid, Container } from '@components/Grid';
 import styled from 'styled-components';
 
@@ -11,25 +12,53 @@ const FullWidth = styled.div`
 `;
 
 const LifeInsurance = ({ title, description, ...props }) => {
+  let isBrowser;
+
+  useEffect(() => {
+    isBrowser = () => {
+      return typeof window !== 'undefined';
+    };
+
+    if (isBrowser) {
+      const policyGeniusURL =
+        'https://www.policygenius.com/price-calculator/calculator.js';
+
+      let policyGeniusScript = document.createElement('script');
+      
+      policyGeniusScript.type = 'text/javascript';
+      policyGeniusScript.src = policyGeniusURL;
+      try {
+        policyGeniusScript.appendChild(document.createTextNode(code));
+        document.body.appendChild(s);
+      } catch (e) {
+        policyGeniusScript.text = policyGeniusURL;
+        document.body.appendChild(policyGeniusScript);
+      }
+    }
+  });
+
   return (
     <>
-      <Layout pageTitle={`${title} | PolicyGenius`} description={description}>
+      <Layout pageTitle={`${title} | Life Insurance`} description={description}>
         <FullWidth bg={'var(--baseDark)'} color={'var(--white)'}>
           <Container>
             <Grid>
               <div className="grid__col grid__col--1-of-2">
-                <h2 style={{ marginTop: '80px' }}>
-                Life Insurance
-                </h2>
+                <h2 style={{ marginTop: '80px' }}>Life Insurance</h2>
                 <p className="lead">
-                  Life insurance coverage provides financial security for you and your loved ones.
+                  Life insurance gives you peace of mind and protects the people
+                  you love.
+                </p>
+                <p>
+                  Shop plans with top insurers and save up to 50%.
+                  <br />
+                  Immediate access to licensed experts to find the best plan.
                 </p>
               </div>
               <div className="grid__col grid__col--1-of-2">
                 <div
                   dangerouslySetInnerHTML={{
-                    __html: `<div id="policygenius-price-calculator" data-offer-id="335" data-aff-id="1819" data-sub-id="organic"></div>
-                             <script src="https://www.policygenius.com/price-calculator/calculator.js" type="text/javascript"></script>`,
+                    __html: `<div id="policygenius-price-calculator" data-offer-id="335" data-aff-id="1819" data-sub-id="organic"></div>`,
                   }}
                   style={{ marginTop: '25px' }}
                 ></div>
